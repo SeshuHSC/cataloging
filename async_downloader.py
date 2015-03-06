7171498587274954a24dce7bb05e9e2aa7ad4024
neo01124 <varunjewalikar@hotmail.com>
2015-03-06 15:43:00 +0100

    async pk downloader added

--- async_downloader.py
+++ async_downloader.py
@@ -251,4 +251,5 @@ def url_resolver(url):
     return res.geturl()
 
 # def download_all_songs(input_file = '', chunk_size = '20'):
-	
+if __name__ == "__main__":
+	download_songs('songsPk_resolved.json', '/mnt/bollywoodCollection', 20)	
