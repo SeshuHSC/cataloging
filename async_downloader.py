86015864f1f15aa9e07f2f4e0f4612678cc2957a
neo01124 <varunjewalikar@hotmail.com>
2015-03-10 19:12:15 +0100

    err fixing

--- async_downloader.py
+++ async_downloader.py
@@ -253,4 +253,4 @@ def url_resolver(url):
 
 # def download_all_songs(input_file = '', chunk_size = '20'):
 if __name__ == "__main__":
-	download_songs('songsPk_resolved.json', '/mnt/bollywoodCollection', 20)	
+	download_songs('songsPk_resolved.json', '/home/ubuntu/audio/bollywoodCollection', 20)	
